14e4b0d6847f68a2c1e46f1103b6700418289fbd
FelipeHR <42549396+FelipeHR@users.noreply.github.com>
2018-09-13 21:35:35 -0300

    Create 1409.cpp

---
+++ 2018_2/aceptados/1409.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int g1;
+	int g2;
+	cin>>g1>>g2;
+    int r1;
+    int r2; 
+    r1=g2-1;
+    r2=g1-1;
+	cout<<r1<<" "<<r2;
+	return 0;
+}
